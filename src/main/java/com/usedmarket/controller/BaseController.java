e68a35d887bc3e621a024c4d50920071e9fadfef
huangMP <675454967@qq.com>
2016-11-29 17:38:06 +0800

    增加 BaseController

--- src/main/java/com/usedmarket/controller/BaseController.java
+++ src/main/java/com/usedmarket/controller/BaseController.java
@@ -6,9 +6,9 @@ import com.usedmarket.dto.HttpResult;
  * Created by huangMP on 2016/11/29.
  * decription :
  */
-public class BaseController<T> {
+public class BaseController {
 
-    public HttpResult getHttpResult(String resultCenter,T data) {
+    public <T> HttpResult getHttpResult(String resultCenter,T data) {
         HttpResult result = new HttpResult(resultCenter,data);
         return result;
     }
