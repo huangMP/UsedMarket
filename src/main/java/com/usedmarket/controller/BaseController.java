3bc871ac2bca7c2864b970edd5a1187f72c22af3
huangMP <675454967@qq.com>
2016-11-29 17:30:19 +0800

    新增 BaseController

---
+++ src/main/java/com/usedmarket/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.usedmarket.controller;
+
+import com.usedmarket.dto.HttpResult;
+
+/**
+ * Created by huangMP on 2016/11/29.
+ * decription :
+ */
+public class BaseController<T> {
+
+    public HttpResult getHttpResult(String resultCenter,T data) {
+        HttpResult result = new HttpResult(resultCenter,data);
+        return result;
+    }
+
+
+}
