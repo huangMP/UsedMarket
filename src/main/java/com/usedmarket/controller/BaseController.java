27862cdf5cb4e1d0e3aa6a9a35257bb36c8b36db
Peivxuan <944184696@qq.com>
2016-11-30 18:53:08 +0800

    v2.8.2： 1. 更新baseController

--- src/main/java/com/usedmarket/controller/BaseController.java
+++ src/main/java/com/usedmarket/controller/BaseController.java
@@ -1,6 +1,7 @@
 package com.usedmarket.controller;
 
 import com.usedmarket.dto.HttpResult;
+import com.usedmarket.util.UuidUtil;
 
 /**
  * Created by huangMP on 2016/11/29.
@@ -8,10 +9,23 @@ import com.usedmarket.dto.HttpResult;
  */
 public class BaseController {
 
-    public <T> HttpResult getHttpResult(String resultCenter,T data) {
-        HttpResult result = new HttpResult(resultCenter,data);
-        return result;
+    protected static final String OPERATION_SUCCESS = "操作成功";
+
+    protected static final String OPERATION_FAILED = "操作失败";
+
+    protected  static final String OPERATION_REPETITION = "操作重复";
+
+    protected  <T> HttpResult getHttpResult(String resultCenter,T data) {
+        return new HttpResult<T>(resultCenter,data);
     }
 
+    protected String get32UUID() {
+        return UuidUtil.get32UUID();
+    }
+
+    protected HttpResult getFrequentlyUsedReturnResultByBool(Boolean b) {
+       return b ?
+        this.getHttpResult(BaseController.OPERATION_SUCCESS,"") : this.getHttpResult(BaseController.OPERATION_FAILED,"");
+    }
 
 }
